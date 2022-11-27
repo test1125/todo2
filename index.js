cd4012bb773a1ddbc2fa81cbc52137e87c882d43
{test} <000.tgtg.000@gmail.com>
2022-11-27 22:11:48 +0900

    削除ボタンの有効化

--- index.js
+++ index.js
@@ -4,13 +4,31 @@ const note = document.querySelector("#note");
 let insertElm="";
 
 function addItem() {
-    const dragIcon = '<td><img id="drag_icon" src="drag_indicator.svg"></img></td>';
+    const dragIcon = '<td><img class="drag_icon" src="drag_indicator.svg"></img></td>';
     let tdTask = `<td>${taskName.value}</td>`;
     const tdComplete = "<td><button>完了</button></td>";
-    const tdDelete = "<td><button>削除</button></td>";
+    const tdDelete = '<td><button class="delete">削除</button></td>';
     insertElm = `<tr>${dragIcon}${tdTask}${tdComplete}${tdDelete}</tr>`;
     note.insertAdjacentHTML("beforeend", insertElm);
     taskName.value="";
 }
 
+function deleteItem(){
+    if (event.target.className == "delete") {
+        let id = event.target.closest("tbody"); //tbodyが作られるのでそこから消す
+        console.log(id);
+        id.remove();
+    }
+
+}
+
 addTask.addEventListener("click", addItem);
+document.addEventListener("click", deleteItem);
+
+
+//リダイレクトを避ける
+document.querySelector("#form_task").addEventListener("submit", (event)=> {
+    event.stopPropagation();
+    event.preventDefault();
+    }
+)
