dc9dbdf6811b41daff165ea87780e0d928ed0534
{test} <000.tgtg.000@gmail.com>
2022-11-27 23:50:54 +0900

    タスクを未/済で分類、完了ボタンの挙動を追加

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const taskName = document.querySelector("#task_name");
 const addTask = document.querySelector("#add_task");
-const note = document.querySelector("#note");
+const todo = document.querySelector("#todo");
+const done = document.querySelector("#done");
 let insertElm="";
 
 function addItem() {
-    const dragIcon = '<td><img class="drag_icon" src="drag_indicator.svg"></img></td>';
-    let tdTask = `<td>${taskName.value}</td>`;
-    const tdComplete = "<td><button>完了</button></td>";
-    const tdDelete = '<td><button class="delete">削除</button></td>';
-    insertElm = `<tr>${dragIcon}${tdTask}${tdComplete}${tdDelete}</tr>`;
-    note.insertAdjacentHTML("beforeend", insertElm);
-    taskName.value="";
+    if (!taskName.value){
+        alert("値が入力されていません。")
+    } else {
+        const dragIcon = '<td><img class="drag_icon" src="drag_indicator.svg"></img></td>';
+        let tdTask = `<td>${taskName.value}</td>`;
+        const tdComplete = '<td><button class="complete">完了</button></td>';
+        const tdDelete = '<td><button class="delete">削除</button></td>';
+        insertElm = `<tr>${dragIcon}${tdTask}${tdComplete}${tdDelete}</tr>`;
+        todo.insertAdjacentHTML("beforeend", insertElm);
+        taskName.value="";
+    }
 }
 
 function deleteItem(){
@@ -18,9 +23,19 @@ function deleteItem(){
         let id = event.target.closest("tbody"); //tbodyが作られるのでそこから消す
         console.log(id);
         id.remove();
+    } else if (event.target.innerText == "完了"){
+        event.target.innerText="戻す"
+        let id = event.target.closest("tbody");
+        id.remove();
+        done.insertAdjacentHTML("beforeend", id.innerHTML);        
+    } else if (event.target.innerText == "戻す") {
+        event.target.innerText="完了";
+        let id = event.target.closest("tbody");
+        id.remove();
+        todo.insertAdjacentHTML("beforeend", id.innerHTML);
     }
-
 }
+
 
 addTask.addEventListener("click", addItem);
 document.addEventListener("click", deleteItem);
