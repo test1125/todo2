3272b518a65d115123a4091fc4b65c5b7f43290e
{test} <000.tgtg.000@gmail.com>
2023-04-04 17:45:13 +0900

    編集機能を追加しました

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const taskName = document.querySelector("#task_name");
-const addTask = document.querySelector("#add_task");
+const taskName = document.querySelector(".add-task__input");
+const addTask = document.querySelector(".add-task__button");
 const todo = document.querySelector("#todo");
 const done = document.querySelector("#done");
-const handle = document.querySelector(".handle");
+const handle = document.querySelector(".item__handle");
 const dummy1 = document.querySelector('#dummy1');
 const dummy2 = document.querySelector('#dummy2');
+const itemInput = document.querySelector(".item__input");
 
 let insertElm="";
 let idNum = 0;
@@ -13,11 +14,18 @@ function addItem() {
     if (!taskName.value){
         alert("値が入力されていません。")
     } else {
-        const dragIcon = '<td class="handle"><img class="drag_icon" src="drag_indicator.svg"></img></td>';
-        let tdTask = `<td>${taskName.value}</td>`;
-        const tdComplete = '<td><button class="complete">完了</button></td>';
-        const tdDelete = '<td><button class="delete">削除</button></td>';
-        insertElm = `<tr id="list${idNum}" draggable="true">${dragIcon}${tdTask}${tdComplete}${tdDelete}</tr>`;
+        insertElm =
+        `<tr id="list${idNum}" class="item" draggable="true">
+            <td class="item__handle">
+                <img class="item__drag_icon" src="drag_indicator.svg">
+            </td>
+            <td class="item__task-name">
+                <input class="item__input" value="${taskName.value}">
+            </td>
+            <td>
+                <button class="item__complete">完了</button></td><td><button class="item__delete">削除</button>
+            </td>
+        </tr>`
         dummy1.insertAdjacentHTML("beforebegin", insertElm);
         taskName.value="";
         idNum++;
@@ -26,11 +34,21 @@ function addItem() {
 
 function DelAndDoneItem(event){
     console.log(event.target);
-    if (event.target.className == "delete") {
+    if (event.target.className == "item__delete") {
         let id = event.target.closest("tr");
         console.log(id);
         id.remove();
-    } 
+    }
+    // 編集完了（Enter押下）時にフォーカスを外す
+    else if(event.target.className == "item__input"){
+        console.log("itemInput");
+        event.target.addEventListener("keydown", e =>{
+            console.log("keydown");
+            if(e.key == "Enter") {
+                e.target.blur();
+            }
+        })
+    }
     else if (event.target.innerText == "完了"){
         event.target.innerText="戻す"
         let clone = event.target.closest("tr").cloneNode(true);
@@ -82,7 +100,7 @@ done.addEventListener("drop", handleDrop);
 
 
 //リダイレクトを避ける
-document.querySelector("#form_task").addEventListener("submit", (event)=> {
+document.querySelector(".add-task__form").addEventListener("submit", (event)=> {
     event.stopPropagation();
     event.preventDefault();
     }
